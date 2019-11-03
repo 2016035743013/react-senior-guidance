e0e0ec8951f3b866a4d8f6436adf9d36b1818c5f
cjhcj <219939754@qq.com>
2019-11-03 09:59:16 +0800

    代码分割

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React from 'react';
-import ReactDom from 'react-dom';
 
-class CustomInput extends React.Component {
-    render() {
-        return (
-            <div>
-                <input type="text" ref={this.props.inputElement} />
-            </div>
-        )
-    }
-    componentDidMount() {
-        this.props.inputElement.current.focus()
-    }
-}
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.input = React.createRef();
-    }
-    render() {
-        return (
-            <>
-                <CustomInput inputElement={this.input} />
-            </>
-        )
-    }
-}
+const Home = lazy(() => import('./routes/Home'));
+const About = lazy(() => import('./routes/About'));
 
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </Switch>
+    </Suspense>
+  </Router>
+);
 export default App;
