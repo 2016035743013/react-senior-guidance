406510b845bd94d9e8824ba02b57819d52ce01b6
cjhcj <219939754@qq.com>
2019-11-03 19:55:53 +0800

    5.refs转发

--- src/App.js
+++ src/App.js
@@ -1,54 +1,24 @@
 import React, { Component } from 'react'
 
 
-class ErrorComponent extends Component {
-    constructor() {
-        super();
-        this.state = {
-            hasError: false
-        }
-    }
-    static getDerivedStateFromError(error) {
-        return {
-            hasError: true
-        }
-    }
-    componentDidCatch(error, errorInfo) {
-        console.log(error, errorInfo);
-    }
 
-    render() {
-        if (this.state.hasError) {
-            // 你可以自定义降级后的 UI 并渲染
-            return <h1>Something went wrong.</h1>;
-        }
-        return this.props.children;
-    }
-}
+// 你可以直接获取 DOM button 的 ref：
 
-export class TestError extends Component {
-    render() {
-
-        const test = 'test';
-        
-        return (
-            <>
-                testerror
-            </>
-        )
-    }
-}
 
 export default class App extends Component {
     constructor() {
         super();
     }
-    
+
     render() {
+        this.inputRef = React.createRef();
         return (
-            <ErrorComponent>
-                <TestError></TestError>
-            </ErrorComponent>
+            <>
+                <input type="text" ref={this.inputRef}/>
+            </>
         )
-    }    
+    }
+    componentDidMount() {
+        console.log(this.inputRef)
+    }
 }
