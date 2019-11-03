48837b4282440a39c8252fe5c9b07377980500f1
cjhcj <219939754@qq.com>
2019-11-03 16:40:06 +0800

    3.context

--- src/App.js
+++ src/App.js
@@ -2,17 +2,82 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React, { Suspense, lazy } from 'react';
 
-const Home = lazy(() => import('./routes/Home'));
-const About = lazy(() => import('./routes/About'));
+// const {Consumer, Provider} = React.createContext('light');
+const ThemeContext = React.createContext('light');
+const UserContext = React.createContext('useragent');
+class Child extends React.PureComponent {
+    // static contextType = UserContext;
+    // static contextType = ThemeContext;
+    render() {
+        console.log(this.context);
+        return (
+            <div>
+                {
+                    <ThemeContext.Consumer>
+                        {
+                            context => {
+                                return (
+                                    <UserContext.Consumer>
+                                        {
+                                            val => { 
+                                                return (
+                                                    <span>{val.user}</span>
+                                                )
+                                            }
+                                        }
+                                    </UserContext.Consumer>
+                                )
+                            }
+                        }
+                    </ThemeContext.Consumer>
+                }
+            </div>
+        )
+    }
+    handleChange = () => {
+        this.context.handle('test');
+    }
+    componentDidMount() {
+    }
+}
 
-const App = () => (
-  <Router>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-      </Switch>
-    </Suspense>
-  </Router>
-);
+class Parent extends React.PureComponent {
+    static contextType = ThemeContext;
+    render() {
+        console.log(this.context);
+        return (
+            <div>
+                <Child />
+            </div>
+        )
+    }
+}
+
+class App extends React.PureComponent {
+    constructor() {
+        super();
+        this.handle = this.handle.bind(this);
+        this.state = {
+            name: 'zhang sheng bin '
+        }
+    }
+    handle(val) {
+        console.log(val);
+        this.setState({
+            name: 'cjhcj'
+        })
+    }
+    render() {
+        return (
+            <div>
+                <UserContext.Provider value={{ user: 'user' }}>
+                    <ThemeContext.Provider value={{ name: this.state.name, handle: this.handle }}>
+                        <Parent />
+                    </ThemeContext.Provider>
+                </UserContext.Provider>
+            </div>
+        )
+    }
+}
+
 export default App;
